54a985b917dfbc641433707b1256e8fb5e5dc85c
Soham Shinde <sohamshinde04@gmail.com>
2023-03-06 04:24:20 +0530

    combined the tag and nonce into the same encrypted file

--- main.py
+++ main.py
@@ -17,36 +17,42 @@ def encrypt_file(file_path, key):
     # key = key.encode('utf-8')
     print(key)
     # Define the AES cipher object
-    cipher = AES.new(key, AES.MODE_EAX)
+    # Generate a random nonce and counter
+    nonce = get_random_bytes(16)
+
+    # Create the AES cipher in EAX mode
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
 
     # Read the entire file into memory
     with open(file_path, "rb") as f:
-        file_contents = f.read()
+        data = f.read()
 
+    # Encrypt the data
+    ciphertext, tag = cipher.encrypt_and_digest(data)
     # Encrypt the file contents
-    encrypted_data, tag = cipher.encrypt_and_digest(file_contents)
+    # encrypted_data, tag = cipher.encrypt_and_digest(file_contents)
 
     # Write the encrypted data and tag to the output file
     with open(file_path, "wb") as f:
-        f.write(encrypted_data)
+        f.write(nonce)
+        f.write(ciphertext)
+        f.write(tag)
         # [f.write(x) for x in (cipher.nonce, tag, encrypted_data)]
 
     return cipher.nonce, tag
     # [f.write(x) for x in (cipher.nonce, tag, encrypted_data)]
 
 
-def decrypt_file(file_path, key, nonce, tag):
-    # Encode the key as bytes
-    # key = key.encode('utf-8')
-    # nonce = get_random_bytes(16)  # Generate a new nonce
-    print("decrypting file ")
-
+def decrypt_file(file_path, key):
     # Read the nonce, ciphertext, and tag from the encrypted file
     with open(file_path, 'rb') as file:
+        nonce = file.read(16)
         ciphertext = file.read()
-        # nonce, ciphertext, tag = [file.read(x) for x in (12, -1, 28)]
+        tag = ciphertext[-16:]
+        ciphertext = ciphertext[:-16]
 
     # Create the AES cipher object with a 256-bit key and GCM mode
+    # counter = Counter.new(128, initial_value=0)
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
 
     # Decrypt the ciphertext and verify the authentication tag
@@ -60,12 +66,11 @@ def decrypt_file(file_path, key, nonce, tag):
 def fun():
     # Asking User for file input
     file_path = filedialog.askopenfile().name
-    # print(file_path)
+
+    block_size = 1024 * 1024
 
     # Splitting the file into blocks and storing the paths to the same
-    block_paths = split_file(file_path, 1048576)
-    # print(block_paths)
-    # print(open(block_paths[0], 'r').read())
+    block_paths = split_file(file_path, block_size)
 
     # Hashing the blocks of the file
     hashes = []
@@ -74,21 +79,15 @@ def fun():
         hash_value = hashFile(block_path)
         hashes.append(hash_value)
 
-    # print(hashes)
-
-    nouce_list = []
-    tags_list = []
+    # Encrypting Blocks with Hash
 
     for block_path, hash in zip(block_paths, hashes):
-        nouce, tag = encrypt_file(block_path, hash)
-        nouce_list.append(nouce);
-        tags_list.append(tag)
+        encrypt_file(block_path, hash)
 
-    print(nouce_list)
-    print(tags_list)
+    # Decrypting the blocks
 
-    for block_path, hash, nonce, tag in zip(block_paths, hashes, nouce_list, tags_list):
-        decrypt_file(block_path, hash, nonce, tag)
+    for block_path, hash in zip(block_paths, hashes):
+        decrypt_file(block_path, hash)
 
     pass
 
